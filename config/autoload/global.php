53e61a245b2663a6722dce850c66ed249eea0b6d
Julio Pescador <jpescador@ucmerced.edu>
2013-07-29 08:49:07 -0700

    moved bjyauthorize config to global.php

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -25,4 +25,83 @@ return array(
                     => 'Zend\Db\Adapter\AdapterServiceFactory',
         ),
     ),
+    'bjyauthorize' => array(
+
+        // set the 'guest' role as default (must be defined in a role provider)
+        'default_role' => 'guest',
+
+        /* this module uses a meta-role that inherits from any roles that should
+         * be applied to the active user. the identity provider tells us which
+         * roles the "identity role" should inherit from.
+         *
+         * for ZfcUser, this will be your default identity provider
+         */
+        'identity_provider'  => 'Permissions\Provider\IdentityProvider',
+
+        /* role providers simply provide a list of roles that should be inserted
+         * into the Zend\Acl instance. the module comes with two providers, one
+         * to specify roles in a config file and one to load roles using a
+         * Zend\Db adapter.
+         */
+        'role_providers' => array(
+
+            /* here, 'guest' and 'user are defined as top-level roles, with
+             * 'admin' inheriting from user
+             */
+            'BjyAuthorize\Provider\Role\Config' => array(
+                'guest' => array(),
+                'user'  => array('children' => array(
+                    'admin' => array(),
+                )),
+            )
+        ),
+
+        // resource providers provide a list of resources that will be tracked
+        // in the ACL. like roles, they can be hierarchical
+        'resource_providers' => array(
+            'BjyAuthorize\Provider\Resource\Config' => array(
+                'adminAccess' => array(),
+                'adminNav' => array(),
+            ),
+        ),
+
+        /* rules can be specified here with the format:
+         * array(roles (array), resource, [privilege (array|string), assertion])
+         * assertions will be loaded using the service manager and must implement
+         * Zend\Acl\Assertion\AssertionInterface.
+         * *if you use assertions, define them using the service manager!*
+         */
+        'rule_providers' => array(
+            'BjyAuthorize\Provider\Rule\Config' => array(
+                'allow' => array(
+                    // allow guests and users (and admins, through inheritance)
+                    // the "wear" privilege on the resource "pants"
+                    //array(array('guest', 'user'), 'pants', 'wear')
+                   array( array('admin'), 'adminAccess', 'view' ),
+                   array( array('admin'), 'adminNav', 'view' )
+                )
+            )
+        ),
+
+        /* Currently, only controller and route guards exist
+         *
+         * Consider enabling either the controller or the route guard depending on your needs.
+         */
+        'guards' => array(
+            /* If this guard is specified here (i.e. it is enabled), it will block
+             * access to all controllers and actions unless they are specified here.
+             * You may omit the 'action' index to allow access to the entire controller
+             */
+            'BjyAuthorize\Guard\Controller' => array(
+                array( 'controller' => 'Application\Controller\Index', 'roles' => array('guest', 'user') ),
+                array( 'controller' => 'Authentication\Controller\Index', 'roles' => array('guest', 'user') ),
+                array( 'controller' => 'Admin\Controller\Index', 'roles' => array('admin') ),
+                array( 'controller' => 'users', 'roles' => array('admin') )
+            ),
+
+        ),
+        
+        // strategy service name for the strategy listener to be used when permission-related errors are detected
+        'unauthorized_strategy' => 'Permissions\View\RedirectionStrategy',
+    ),
 );
